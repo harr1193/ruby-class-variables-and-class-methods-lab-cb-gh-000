359ffed44a4b82c826f3f428d125fd3b9613f546
Kyle Harris <k_harris95@live.com>
2018-01-30 19:04:06 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.collect { |genre| return genre if !@@genre.include?(genre) }
+  end
+
+  def self.artists
+    @@artists.collect { |artist| return artist if !@@artists.include?(artist)  }
+  end
+end

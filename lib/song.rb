bdccd2876a1056130dc213a1c216fb6b055acc06
Kyle Harris <k_harris95@live.com>
2018-01-30 20:44:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,14 @@ class Song
   def self.artists
     @@artists.uniq
   end
+
+  def genre_count
+    genre_hash = Hash.new
+    @@genres.each do |genre|
+      if genre_hash[genre] == nil
+        genre_hash[genre] = 1
+      else
+        genre_hash[genre] += 1
+    end
+  end
 end
